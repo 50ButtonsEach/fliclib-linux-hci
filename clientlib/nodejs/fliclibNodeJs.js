a55947475e56e72b2ce5a24afcb15ad1a5a0569a
Emill <emil.lenngren@gmail.com>
2017-09-06 13:38:21 +0200

    Update fliclibNodeJs.js
    Fix latencyMode and autoDisconnectTime in fliclibNodeJs.js

--- clientlib/nodejs/fliclibNodeJs.js
+++ clientlib/nodejs/fliclibNodeJs.js
@@ -471,8 +471,8 @@ var FlicConnectionChannel = (function() {
 	
 	return function(bdAddr, options) {
 		options = options || {};
-		var latencyMode = (latencyMode in options) ? options.latencyMode : "NormalLatency";
-		var autoDisconnectTime = (autoDisconnectTime in options) ? options.autoDisconnectTime : 511;
+		var latencyMode = ("latencyMode" in options) ? options.latencyMode : "NormalLatency";
+		var autoDisconnectTime = ("autoDisconnectTime" in options) ? options.autoDisconnectTime : 511;
 		
 		EventEmitter.call(this);
 		var id = counter++;
